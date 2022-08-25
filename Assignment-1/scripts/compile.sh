115d34ffbe5d57c3bc5ca2b810cff37d3ac0e789
SociallyAwake <siddharth000coool@gmail.com>
2022-08-26 02:50:58 +0530

    plot file added

--- Assignment-1/scripts/compile.sh
+++ Assignment-1/scripts/compile.sh
@@ -1,3 +1,5 @@
-g++ -std=c++11 -w -Iinclude/ src/main.cpp -o obj/main.o
-g++ -w -Iinclude/ test/io_handler.cpp -o obj/io_handler.o
+g++ -std=c++11 -O3 -w -Iinclude/ src/main.cpp -o obj/main.o
+g++ -std=c++11 -O3 -w -Iinclude/ src/plot.cpp -o obj/plot.o
+
+
 
