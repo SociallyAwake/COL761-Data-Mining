478ddc34cfdf39e89948243cefecfe2945b958d0
Manish Kumar Srivastava <siddharth000coool@gmail.com>
2022-08-26 20:52:03 +0530

    minor fixes

--- Assignment-1/install.sh
+++
@@ -1,4 +0,0 @@
-git clone https://github.com/SociallyAwake/COL761-Data-Mining
-cd COL761-Data-Mining/
-unzip *.zip
-cd 
--- Assignment-1/test.sh
+++
@@ -1,7 +0,0 @@
-name="ismail"
- 
-if [ "$name" != "ismail" ]; then
-    echo "Both Strings are NOT Equal."
-else
-    echo "Both Strings are Equal."
-fi
