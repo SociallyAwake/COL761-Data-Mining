8b3f5dc55425f7e24ade5eedd6d5136d8cfc16d1
SociallyAwake <siddharth000coool@gmail.com>
2022-10-09 14:48:26 +0530

    zip changed

--- Assignment-2/compile.sh
+++ Assignment-2/compile.sh
@@ -1,2 +1,6 @@
+wget https://boostorg.jfrog.io/artifactory/main/release/1.80.0/source/boost_1_80_0.zip
+unzip boost_1_80_0.zip
+cp -r boost_1_80_0/boost ./include/
+rm -rf boost_1_80_0 boost_1_80_0.zip
 make clean
 make all
--- MCS212138_install.sh
+++ MCS212138_install.sh
@@ -1,8 +1,2 @@
 git clone https://github.com/SociallyAwake/COL761-Data-Mining
 cd COL761-Data-Mining/
-unzip *.zip
-cd HW2_MCS212138
-wget https://boostorg.jfrog.io/artifactory/main/release/1.80.0/source/boost_1_80_0.zip
-unzip boost_1_80_0.zip
-cp -r boost_1_80_0/boost ./include/
-rm -rf boost_1_80_0 boost_1_80_0.zip
